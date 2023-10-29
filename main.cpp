42e4411d880d5a38b561f62e59dabfc50da7908c
Julius <juliuswolf11@icloud.com>
2023-10-29 17:12:06 +0100

    Main

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
  *  Created on: 29.10.2023
  *      Author: julius
  */
+#include "Fahrzeug.hpp"
+#include "global.hpp"
+#include "PKW.hpp"
+#include "Fahrrad.hpp"
+#include <stdio.h>
+
 
 int main()
 {
-
+	return 1;
 }
-
 
